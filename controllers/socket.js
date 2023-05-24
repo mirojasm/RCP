ec8cf1e0b28c33caa4b0c0aad6b98076ad30af15
Sebastian Gallardo <ext_sagallardol@falabella.cl>
2023-05-24 11:26:08 -0400

    second commint

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,28 +1,25 @@
-
 /*
  * Scokets listener.
  * Este controlador se encarga de manejar las conexiones, escuchar los eventos y delegar
  * la responsabilidad a quien corresponda.
  */
 
-module.exports = function(server) {
-    var io = require('socket.io').listen(server);
-    io.sockets.on('connection', function (socket) {
-
-
-    	socket.on('startGame', function(data){
-    		// console.log(data);
-    		socket.emit('instructionGame', { msg: 'reply to event'} );
-    	});
-
-        socket.on('event', function (data) {
-            // console.log(data);
-            socket.emit('reply-event', { msg: 'reply to event'} );
-        });
-
-        //Podemos dividir en controladores de sockets, pasandoles io y socket
-        //como se indica acontinuación:
-        //require('../controllers/shoutbox.js')(io, socket);
-        require('../controllers/gameAquarium.js')(io, socket);
+module.exports = function (server) {
+  var io = require("socket.io").listen(server);
+  io.sockets.on("connection", function (socket) {
+    socket.on("startGame", function (data) {
+      // console.log(data);
+      socket.emit("instructionGame", { msg: "reply to event" });
     });
-}
+
+    socket.on("event", function (data) {
+      // console.log(data);
+      socket.emit("reply-event", { msg: "reply to event" });
+    });
+
+    //Podemos dividir en controladores de sockets, pasandoles io y socket
+    //como se indica acontinuación:
+    //require('../controllers/shoutbox.js')(io, socket);
+    require("../controllers/gameAquarium.js")(io, socket);
+  });
+};
