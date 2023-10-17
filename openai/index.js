4d608a60e21aa26d7d3529e80afb03106e873b60
niyetsin <camilo.rubilar@mail.udp.cl>
2023-10-17 05:23:20 -0300

    Update of socket library & ChatGPT bots

--- openai/index.js
+++ openai/index.js
@@ -1,12 +1,12 @@
-const { response } = require("express");
+//const { response } = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const config = new Configuration({
   apiKey: "" // Reemplaza "TU_CLAVE_DE_API_AQUI" con tu propia clave de API
 });
 const openai1 = new OpenAIApi(config); // Primer asistente
 const openai2 = new OpenAIApi(config); // Segundo asistente
-let acuerdo1=1;
-let acuerdo2=0;
+let acuerdo1=2;
+let acuerdo2=3;
 
 
 // Instrucciones de cada agente
@@ -50,9 +50,10 @@ const SYSTEM_MESSAGE1 = {
   // FORMATO
 
 	"\nTodas tus respuestas deben estar en formato Json, sin desviaciones"+
-  "\nEl objeto Json tiene que tener este formato: [{'q':'string','a':'string'}].\n\n" +
-	"Donde 'q' corresponde a los mensajes del agente virtual, mientras que 'a' corresponde a el mensaje escrito por el usuario"+
-	"\nTu mensaje de respuesta sea corto y solo responde en formato Json"
+	"\nTu mensaje de respuesta sea corto y solo responde en formato Json"+
+  // PRUEBA PARA ALUCINACIONES DE CHATGPT Y EVITAR QUE UTILICE FUNCIONES
+  // QUE NO HAN SIDO CREADAS
+  "\nOnly use the functions you have been provided with."
 };
 
 const SYSTEM_MESSAGE2 = {
@@ -93,7 +94,11 @@ const SYSTEM_MESSAGE2 = {
 
   // FORMATO
 
-  "\nTu mensaje de respuesta sea corto y solo responde en formato Json"
+	"\nTodas tus respuestas deben estar en formato Json, sin desviaciones"+
+	"\nTu mensaje de respuesta sea corto y solo responde en formato Json"+
+  // PRUEBA PARA ALUCINACIONES DE CHATGPT Y EVITAR QUE UTILICE FUNCIONES
+  // QUE NO HAN SIDO CREADAS
+  "\nOnly use the functions you have been provided with."
 };
 
 // ---------- ETAPAS ------------
@@ -122,7 +127,7 @@ const etapas = {
 
 // CIERRE_Y_SATISFACCION
 
-//  Hay que añadir funciones que permitan realizar el flujo de conversación de manera correcta
+// Hay que añadir funciones que permitan realizar el flujo de conversación de manera correcta
 // Funcion para agreement entre agentes
 // Funcion para identificar etapas y eventos
 const funciones = [
@@ -170,14 +175,19 @@ const funciones = [
           "properties": {
               "mensaje": {
                   "type": "string",
-                  "description": "Respuesta de saludo a Usuario y preséntate",
+                  "description": "responder con mensaje: 'Ok, estamos de acuerdo'",
               },              
               "Etapa": {
                 "type": "string",
                 "description": "Nombre de la funcion/etapa"
-            }
+              },
+              "Acuerdo": {
+                "type": "integer",
+                "description": "1 si están de acuerdo, 0 si no lo están "
+
+              }
           },
-          "required": ["mensaje", "Etapa"],
+          "required": ["mensaje", "Etapa", "Acuerdo"],
       },
   },
 /*  {
@@ -254,28 +264,19 @@ const generateResponseFromMessages1 = async (messages) => {
     id: response.data.id,
     model: response.data.model,
   });
-/*
-  const user={
-    User: messages[0].content
+
+// Verificación de estado acuerdo
+  var mensaje = response.data.choices[0].message.function_call.arguments;
+  if(mensaje.Etapa === "Acuerdo y Selección"){
+    if(mensaje.Acuerdo){
+      acuerdo1 = 1;
+      console.log("Peta esta de acuerdo");
+    }
   }
-  const peta={
-    Peta: response.data.choices[0].message.content
-  }
-  chat.push(user);
-  chat.push(peta);
-  //console.log("Chat"+chat);
-  //console.log(JSON.parse(response.data.choices[0].message.content));
   if(acuerdo1===acuerdo2){
     console.log("Acuerdo Total")
     return "Acuerdo";
-  } 
-
-  if(response.data.choices[0].message.content==="{\"agreement\": true}"){
-    console.log(response.data.choices[0].message.content);
-    acuerdo1="acuerdo";
-    return "ok llegamos acuerdo";
   }
- */
   const respuesta = JSON.parse(response.data.choices[0].message.function_call.arguments)
   return respuesta.mensaje;
 };
@@ -285,6 +286,8 @@ const generateResponseFromMessages2 = async (messages) => {
     model: "gpt-3.5-turbo",
     messages: [SYSTEM_MESSAGE2].concat(messages),
     temperature: 0,
+    functions: funciones,
+    function_call: "auto",
   });
   
   console.log({
@@ -292,26 +295,18 @@ const generateResponseFromMessages2 = async (messages) => {
     id: response.data.id,
     model: response.data.model,
   });
-  /*
-  const user={
-    User: messages[0].content
+// Verificación de estado acuerdo
+  var mensaje = response.data.choices[0].message.function_call.arguments;
+  if(mensaje.Etapa === "Acuerdo y Selección"){
+    if(mensaje.Acuerdo){
+      acuerdo2 = 1;
+      console.log("Peta esta de acuerdo");
+    }
   }
-  const zeta={
-    Zeta: response.data.choices[0].message.content
-  }
-  chat.push(user);
-  chat.push(zeta);
-  //console.log(chat);
   if(acuerdo1===acuerdo2){
     console.log("Acuerdo Total")
     return "Acuerdo";
   } 
-  if(response.data.choices[0].message.content==="{\"agreement\": true}"){
-    console.log(response.data.choices[0].message.content);
-    acuerdo2="acuerdo";
-    return "ok llegamos acuerdo";
-  }
-  */
   return response.data.choices[0].message.content;
 };
 
